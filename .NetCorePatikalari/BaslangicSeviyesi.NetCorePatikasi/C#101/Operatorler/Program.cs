377aefc7508c3a4c3d0de9ff070f5d62375a7a1a
DESKTOP-LCODGMO\hayda <haydarcancoskun92@gmail.com>
2022-07-30 09:20:29 +0300

    C#101 yeni proje eklenmiştir.

---
+++ .NetCorePatikalari/BaslangicSeviyesi.NetCorePatikasi/C#101/Operatorler/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+public class Opetorler
+
+{
+
+    public static void Main(string[] args)
+
+    {
+
+        //Atama İşlemleri
+
+        int x=3;
+
+        int y=3;
+
+        y = y+2;
+
+        Console.WriteLine(y);
+
+        y+=2;
+
+        Console.WriteLine(y);
+
+        y/=1;
+
+        Console.WriteLine(y);
+
+        x*=2;
+
+        Console.WriteLine(x);
+
+        //Mantıksal Operatörler
+
+        bool isSuccess = true;
+
+        bool isCompleted = false;
+
+        if(isSuccess && isCompleted)
+
+        {
+
+            Console.WriteLine("Perfect!");
+
+        }
+
+        if(isSuccess || isCompleted)
+
+        {
+
+            Console.WriteLine("Great!");
+
+        }
+
+         if(isSuccess || !isCompleted)
+
+        {
+
+            Console.WriteLine("Fine!");
+
+        }
+
+        //İlişkisel Operatörler
+
+        int a=3;
+
+        int b=4;
+
+        bool sonuc = a<b;
+
+        Console.WriteLine(sonuc);
+
+        sonuc = a>b;
+
+        Console.WriteLine(sonuc);
+
+        sonuc = a>=b;
+
+        Console.WriteLine(sonuc);
+
+        sonuc = a<=b;
+
+        Console.WriteLine(sonuc);
+
+        sonuc = a==b;
+
+        Console.WriteLine(sonuc);
+
+        sonuc = a!=b;
+
+        Console.WriteLine(sonuc);
+
+        Console.WriteLine("*** Aritmatik Operatörler ***");
+
+
+        int sayi1=10;
+
+        int sayi2=5;
+
+        int sonuc1 = sayi1/sayi2;
+
+        Console.WriteLine(sonuc1);
+
+        sonuc1 = sayi1+sayi2;
+
+        Console.WriteLine(sonuc1);
+
+        sonuc1 = sayi1-sayi2;
+
+        Console.WriteLine(sonuc1);
+
+        sonuc1= sayi1++;
+
+        Console.WriteLine(sonuc1);
+
+        //%:mod alır
+
+       int sonuc2 = 20%3;
+
+        Console.WriteLine(sonuc2);
+
+    }
+
+}
--- .NetCorePatikalari/BaslangicSeviyesi.NetCorePatikasi/ObjectOrientedProgramming/UniversiteYonetimProjesi/README.md
+++ .NetCorePatikalari/BaslangicSeviyesi.NetCorePatikasi/ObjectOrientedProgramming/UniversiteYonetimProjesi/README.md
@@ -3,7 +3,9 @@
 Patika.dev Başlangıç Seviyesi .Net Core Patikasında bulunan Object Oriented Programming dersi kapsamında [Üniversite Yönetim Projesi](https://app.patika.dev/courses/oop/odev-university) için oluşturduğum dosyadır.
 
 # Proje
+
 Üniversite Yönetim Projesi
+
 1. Üniversiteye ait sınıflıklar, çalışma ofisleri ve departmanlar vardır.
 2. Departmanlara ait ofisler vardır.
 3. Üniversiteye ait çalışanlar vardır. Bu çalışanlar profesör veya memur olabilir.
@@ -13,3 +15,5 @@ Bu sistemi tasvir eden Class (Sınıf) diyagramını çiziniz.
 Not : Sınıflara ait nitelik ve davranışların belirtilmesine gerek yoktur.
 
 # Çözüm
+
+Draw.io ile çizdiğim diyagramın orjinal haline buradan ulaşabilirsiniz.
