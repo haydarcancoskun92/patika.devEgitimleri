00ba8ee27d1adcdabed007013a9289a29b1c41a3
DESKTOP-LCODGMO\hayda <haydarcancoskun92@gmail.com>
2022-07-30 09:18:48 +0300

    C#101 ve OOP içerisine yeni projeler eklenmiştir

---
+++ .NetCorePatikalari/BaslangicSeviyesi.NetCorePatikasi/C#101/TipDonusumleri/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+public class TipDonusumleri
+
+{
+
+    public static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("***Implicit Conversion ***");
+
+        byte a = 5;
+
+        sbyte b = 30;
+
+        short c = 10;
+
+        int d = a + b + c;
+
+        Console.WriteLine("d: "+d);
+
+        long h = d;
+
+        Console.WriteLine("h: "+h);
+
+        float l = h;
+
+        Console.WriteLine("l: "+l);
+
+        string e ="Haydar Can";
+
+        char f='k';
+
+        object g = e + f +d;
+
+        Console.WriteLine("g: "+g);
+
+        Console.WriteLine("***Explicit Conversion ***");
+
+        int x =4;
+
+        byte y = (byte)x;
+
+        Console.WriteLine("y: "+y);
+
+        int z = 100;
+
+        byte t = (byte)z;
+
+        Console.WriteLine("t: "+t);
+
+        float w = 10.3f;
+
+        byte v = (byte)w;
+
+        Console.WriteLine("v: "+v);
+
+        Console.WriteLine("***ToString Metodu***");
+
+        int xx=6;
+
+        string yy = xx.ToString();
+
+        Console.WriteLine("yy: "+yy);
+
+        string zz = 12.5f.ToString();
+
+        Console.WriteLine("zz: "+zz);
+
+        Console.WriteLine("***System Convert Sınıfı***");
+
+        string s1="10", s2="20";
+
+        int sayi1,sayi2;
+
+        int toplam;
+
+        sayi1 = Convert.ToInt32(s1);
+
+        sayi2 = Convert.ToInt32(s2);
+
+        toplam = sayi1+sayi2;
+
+        Console.WriteLine("Toplam: "+toplam);
+
+        Console.WriteLine("***Parse");
+
+        ParseMethod();
+    }
+
+   
+
+    public static void ParseMethod()
+
+    {
+
+        string metin1 = "10";
+
+        string metin2 = "10.25";
+
+        int rakam1;
+
+        double double1;
+
+        rakam1 = Int32.Parse(metin1);
+
+        double1 = Double.Parse(metin2);
+
+        Console.WriteLine("Rakam1: "+rakam1);
+
+        Console.WriteLine("Double1: "+double1);
+
+    }
+
+}
+
+ 
