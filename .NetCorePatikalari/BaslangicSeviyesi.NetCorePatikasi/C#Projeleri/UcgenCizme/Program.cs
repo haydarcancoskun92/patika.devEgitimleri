eced657f1b13c7ff91821f558b3eff5ba0d25f0b
DESKTOP-LCODGMO\hayda <haydarcancoskun92@gmail.com>
2022-08-10 22:29:52 +0300

    Yeni dersler ve projeler eklenmiştir.

---
+++ .NetCorePatikalari/BaslangicSeviyesi.NetCorePatikasi/C#Projeleri/UcgenCizme/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class UcgenCizdirme
+
+{
+
+    public static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("Çizilecek üçgen için taban değerini giriniz:");
+
+        int length = Convert.ToInt32( Console.ReadLine());
+
+        UcgenCiz(length);
+
+    }
+
+   
+
+    public static void UcgenCiz(int length)
+
+    {
+
+        for(int i = length;i>0;i--)
+
+        {
+
+            for(int j=0; j<=length-i;j++)
+
+            {
+
+                Console.Write("*");
+
+            }
+
+            Console.WriteLine("");
+
+           
+
+        }
+
+    }
+
+}
