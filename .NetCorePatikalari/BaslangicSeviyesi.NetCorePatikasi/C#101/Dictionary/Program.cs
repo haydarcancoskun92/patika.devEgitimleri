bff52e268b9bfd5786ac057e84720229c84ad85f
DESKTOP-LCODGMO\hayda <haydarcancoskun92@gmail.com>
2022-08-20 11:02:41 +0300

    Yeni Dersler Eklendi

---
+++ .NetCorePatikalari/BaslangicSeviyesi.NetCorePatikasi/C#101/Dictionary/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+
+    public static void Main(string[] args)
+    {
+
+        //System.Collections.Generic
+        Dictionary<int, string> kullanicilar = new Dictionary<int, string>();
+
+        kullanicilar.Add(10, "Ayşe Yılmaz");
+        kullanicilar.Add(12, "Ahmet Yılmaz");
+        kullanicilar.Add(18, "Deniz Arda");
+        kullanicilar.Add(20, "Özcan Coşar");
+
+        //Dictionary Elemanlarına Erişim
+        Console.WriteLine("***Dictionary Elemanlarına Erişim***");
+        Console.WriteLine(kullanicilar[12]);
+
+        foreach (var kullanici in kullanicilar)
+        {
+            Console.WriteLine(kullanici);
+        }
+
+        //Count
+        Console.WriteLine("***Count***");
+        Console.WriteLine(kullanicilar.Count);
+
+        //Contains
+        Console.WriteLine("***Contains***");
+        Console.WriteLine(kullanicilar.ContainsKey(12));
+        Console.WriteLine(kullanicilar.ContainsValue("Haydar Can Coşkun"));
+
+        //Remove
+        Console.WriteLine("***Remove***");
+        kullanicilar.Remove(12);
+
+        foreach (var kullanici in kullanicilar)
+        {
+            Console.WriteLine(kullanici);
+        }
+
+        //Keys
+        Console.WriteLine("***Keys***");
+
+        foreach (var kullanici in kullanicilar.Keys)
+        {
+            Console.WriteLine(kullanici);
+        }
+
+        //Values
+        Console.WriteLine("***Values***");
+
+        foreach (var kullanici in kullanicilar.Values)
+        {
+            Console.WriteLine(kullanici);
+        }
+
+    }
+
+}
